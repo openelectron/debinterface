f266ceed4156d5338acd93ec14a630b9cf8fbba1
Nathan Mustaki <feydaykyn@gmail.com>
2016-07-19 09:29:21 +0200

    CHANGED : toolutils : safe_subprocess : enforce shell command being an array

--- toolutils.py
+++ toolutils.py
@@ -6,10 +6,23 @@ import subprocess
 
 
 def safe_subprocess(command_array):
-    ''' return True/False, command output '''
+    '''Executes shell command. Do not raise
+
+        Args:
+            command_array (list): ideally an array of string elements, but
+                may be a string. Will be converted to an array of strings
+
+        Returns:
+            True/False, command output
+    '''
 
     try:
-        return True, subprocess.check_output(command_array,
+        # Ensure all args are strings
+        if isinstance(command_array, list):
+            command_array_str = [str(x) for x in command_array]
+        else:
+            command_array_str = [str(command_array)]
+        return True, subprocess.check_output(command_array_str,
                                              stderr=subprocess.STDOUT)
     except OSError as e:
         return False, e.__str__()
